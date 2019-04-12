5467527db39e280cbb215f624bb8aac57f6a3966
Elkien3 <Elkien3@users.noreply.github.com>
2019-04-12 09:52:15 -0500

    add crafts

--- init.lua
+++ init.lua
@@ -19,6 +19,15 @@ gunslinger.register_gun("gunslinger_rangedweapons:ak47", {
 	ammo = "gunslinger_rangedweapons:mag_ak47",
 	fire_sound = "rangedweapons_smg"
 })
+minetest.register_craft({
+	output = 'gunslinger_rangedweapons:ak47 1 65534',
+	recipe = {
+		{'default:diamond', 'default:steel_ingot', 'default:tree'},
+		{'default:tree', 'default:mese', 'default:steel_ingot'},
+		{'default:steel_ingot', '', 'default:tree'},
+	}
+})
+
 gunslinger.register_gun("gunslinger_rangedweapons:g36", {
 	itemdef = {
 		description = "G36 Rifle",
@@ -40,6 +49,15 @@ gunslinger.register_gun("gunslinger_rangedweapons:g36", {
 	ammo = "gunslinger_rangedweapons:mag_stanag",
 	fire_sound = "rangedweapons_smg"
 })
+minetest.register_craft({
+	output = 'gunslinger_rangedweapons:g36 1 65534',
+	recipe = {
+		{'default:diamond', 'default:mese', 'default:diamond'},
+		{'default:steel_ingot', 'default:diamond', 'default:steel_ingot'},
+		{'default:steel_ingot', '', 'default:steel_ingot'},
+	}
+})
+
 gunslinger.register_gun("gunslinger_rangedweapons:m16", {
 	itemdef = {
 		description = "M16 Rifle",
@@ -61,6 +79,15 @@ gunslinger.register_gun("gunslinger_rangedweapons:m16", {
 	ammo = "gunslinger_rangedweapons:mag_stanag",
 	fire_sound = "rangedweapons_smg"
 })
+minetest.register_craft({
+	output = 'gunslinger_rangedweapons:m16 1 65534',
+	recipe = {
+		{'default:diamond', 'default:steelblock', 'default:steel_ingot'},
+		{'default:steel_ingot', 'default:diamond', 'default:steel_ingot'},
+		{'default:steel_ingot', '', 'default:steel_ingot'},
+	}
+})
+
 gunslinger.register_gun("gunslinger_rangedweapons:scar", {
 	itemdef = {
 		description = "SCAR Rifle",
@@ -81,6 +108,14 @@ gunslinger.register_gun("gunslinger_rangedweapons:scar", {
 	horizontal_recoil = 10,
 	ammo = "gunslinger_rangedweapons:mag_stanag",
 	fire_sound = "rangedweapons_smg"
+})
+minetest.register_craft({
+	output = 'gunslinger_rangedweapons:scar 1 65534',
+	recipe = {
+		{'default:diamond', 'default:mese', 'default:mese'},
+		{'default:steel_ingot', 'default:steel_ingot', 'default:mese'},
+		{'homedecor:plastic_sheeting', '', 'default:mese_crystal'},
+	}
 })
 
 --Submachine Guns
@@ -104,6 +139,15 @@ gunslinger.register_gun("gunslinger_rangedweapons:vector", {
 	ammo = "gunslinger_rangedweapons:mag_smg",
 	fire_sound = "rangedweapons_smg"
 })
+minetest.register_craft({
+	output = 'gunslinger_rangedweapons:vector 1 65534',
+	recipe = {
+		{'homedecor:plastic_sheeting', 'homedecor:plastic_sheeting', 'homedecor:plastic_sheeting'},
+		{'default:gold_ingot', 'default:mese_crystal', 'homedecor:plastic_sheeting'},
+		{'homedecor:plastic_sheeting', 'default:gold_ingot', ''},
+	}
+})
+
 gunslinger.register_gun("gunslinger_rangedweapons:mp5", {
 	itemdef = {
 		description = "MP5 Submachinegun",
@@ -123,6 +167,15 @@ gunslinger.register_gun("gunslinger_rangedweapons:mp5", {
 	ammo = "gunslinger_rangedweapons:mag_smg",
 	fire_sound = "rangedweapons_smg"
 })
+minetest.register_craft({
+	output = 'gunslinger_rangedweapons:mp5 1 65534',
+	recipe = {
+		{'default:steel_ingot', 'default:steel_ingot', 'default:steel_ingot'},
+		{'default:steel_ingot', 'default:diamond', 'default:steel_ingot'},
+		{'default:steel_ingot', 'homedecor:plastic_sheeting', 'dye:black'},
+	}
+})
+
 gunslinger.register_gun("gunslinger_rangedweapons:mp40", {
 	itemdef = {
 		description = "MP40 Submachinegun",
@@ -142,6 +195,15 @@ gunslinger.register_gun("gunslinger_rangedweapons:mp40", {
 	ammo = "gunslinger_rangedweapons:mag_smg",
 	fire_sound = "rangedweapons_smg"
 })
+minetest.register_craft({
+	output = 'gunslinger_rangedweapons:mp40 1 65534',
+	recipe = {
+		{'default:steel_ingot', 'default:steel_ingot', 'default:steel_ingot'},
+		{'default:steel_ingot', 'default:mese_crystal', 'default:steel_ingot'},
+		{'default:steel_ingot', 'default:mese_crystal_fragment', ''},
+	}
+})
+
 gunslinger.register_gun("gunslinger_rangedweapons:ump", {
 	itemdef = {
 		description = "UMP Submachinegun",
@@ -161,6 +223,15 @@ gunslinger.register_gun("gunslinger_rangedweapons:ump", {
 	ammo = "gunslinger_rangedweapons:mag_smg",
 	fire_sound = "rangedweapons_smg"
 })
+minetest.register_craft({
+	output = 'gunslinger_rangedweapons:ump 1 65534',
+	recipe = {
+		{'default:steel_ingot', 'default:steel_ingot', 'default:steel_ingot'},
+		{'default:steel_ingot', 'default:diamond', 'default:steel_ingot'},
+		{'default:steel_ingot', '', 'default:steel_ingot'},
+	}
+})
+
 gunslinger.register_gun("gunslinger_rangedweapons:uzi", {
 	itemdef = {
 		description = "UZI Submachinegun",
@@ -180,6 +251,14 @@ gunslinger.register_gun("gunslinger_rangedweapons:uzi", {
 	magazine = true,
 	ammo = "gunslinger_rangedweapons:mag_smg",
 	fire_sound = "rangedweapons_smg"
+})
+minetest.register_craft({
+	output = 'gunslinger_rangedweapons:uzi 1 65534',
+	recipe = {
+		{'default:steel_ingot', 'default:steel_ingot', 'default:steel_ingot'},
+		{'default:diamond', 'homedecor:plastic_sheeting', 'default:steel_ingot'},
+		{'', 'default:steel_ingot', ''},
+	}
 })
 
 --Machineguns
@@ -203,6 +282,15 @@ gunslinger.register_gun("gunslinger_rangedweapons:m60", {
 	ammo = "gunslinger_rangedweapons:mag_m60",
 	fire_sound = "rangedweapons_machinegun"
 })
+minetest.register_craft({
+	output = 'gunslinger_rangedweapons:m60 1 65534',
+	recipe = {
+		{'default:diamond', 'default:steel_ingot', 'default:mese'},
+		{'default:steel_ingot', 'default:steelblock', 'default:steelblock'},
+		{'dye:black', 'default:diamond', 'default:steel_ingot'},
+	}
+})
+
 gunslinger.register_gun("gunslinger_rangedweapons:rpk", {
 	itemdef = {
 		description = "RPK Machinegun",
@@ -223,6 +311,14 @@ gunslinger.register_gun("gunslinger_rangedweapons:rpk", {
 	magazine = true,
 	ammo = "gunslinger_rangedweapons:mag_rpk",
 	fire_sound = "rangedweapons_machinegun"
+})
+minetest.register_craft({
+	output = 'gunslinger_rangedweapons:rpk 1 65534',
+	recipe = {
+		{'default:diamond', 'default:mese', ''},
+		{'default:steel_ingot', 'default:steelblock', 'default:tree'},
+		{'', 'default:diamond', 'default:steel_ingot'},
+	}
 })
 
 --Shotguns
@@ -246,7 +342,15 @@ gunslinger.register_gun("gunslinger_rangedweapons:benelli", {
 	ammo = "gunslinger_rangedweapons:bullet_12g 8",
 	fire_sound = "rangedweapons_shotgun_shot"
 })
-gunslinger.register_gun("gunslinger_rangedweapons:rangedweapons_remington", {
+minetest.register_craft({
+	output = "gunslinger_rangedweapons:benelli 1 65534",
+	recipe = {
+		{"default:steel_ingot", "default:diamond", "default:steelblock"},
+		{"homedecor:plastic_sheeting", "default:diamond", "default:steel_ingot"},
+	}
+})
+
+gunslinger.register_gun("gunslinger_rangedweapons:remington", {
 	itemdef = {
 		description = "Remington Shotgun",
 		inventory_image = "rangedweapons_remington.png",
@@ -265,6 +369,14 @@ gunslinger.register_gun("gunslinger_rangedweapons:rangedweapons_remington", {
 	ammo = "gunslinger_rangedweapons:bullet_12g",
 	fire_sound = "rangedweapons_shotgun_shot"
 })
+minetest.register_craft({
+	output = "gunslinger_rangedweapons:remington 1 65534",
+	recipe = {
+		{"default:steel_ingot", "default:steel_ingot", "default:steel_ingot"},
+		{"default:tree", "default:mese_crystal", "default:tree"},
+	}
+})
+
 gunslinger.register_gun("gunslinger_rangedweapons:spas12", {
 	itemdef = {
 		description = "SPAS12 Shotgun",
@@ -283,6 +395,14 @@ gunslinger.register_gun("gunslinger_rangedweapons:spas12", {
 	pellets = 6,
 	ammo = "gunslinger_rangedweapons:bullet_12g 8",
 	fire_sound = "rangedweapons_shotgun_shot"
+})
+minetest.register_craft({
+	output = "gunslinger_rangedweapons:spas12 1 65534",
+	recipe = {
+		{"", "default:steel_ingot", "default:steel_ingot"},
+		{"default:steel_ingot", "default:mese", "default:diamond"},
+		{"homedecor:plastic_sheeting", "default:diamond", "default:steel_ingot"},
+	}
 })
 
 --Sniper Rifles
@@ -308,6 +428,15 @@ gunslinger.register_gun("gunslinger_rangedweapons:awp", {
 	ammo = "gunslinger_rangedweapons:bullet_308mm",
 	fire_sound = "rangedweapons_rifle_a"
 })
+minetest.register_craft({
+	output = 'gunslinger_rangedweapons:awp 1 65534',
+	recipe = {
+		{'default:steel_ingot', 'default:diamondblock', 'default:steel_ingot'},
+		{'default:steel_ingot', 'default:steel_ingot', 'default:steel_ingot'},
+		{'dye:dark_green', 'default:diamond', 'homedecor:plastic_sheeting'},
+	}
+})
+
 gunslinger.register_gun("gunslinger_rangedweapons:m200", {
 	itemdef = {
 		description = "M200 Sniper Rifle",
@@ -330,6 +459,15 @@ gunslinger.register_gun("gunslinger_rangedweapons:m200", {
 	ammo = "gunslinger_rangedweapons:mag_sniper",
 	fire_sound = "rangedweapons_rifle_b"
 })
+minetest.register_craft({
+	output = 'gunslinger_rangedweapons:m200 1 65534',
+	recipe = {
+		{'default:diamondblock', 'default:steel_ingot', 'default:diamondblock'},
+		{'default:steel_ingot', 'default:steel_ingot', 'default:steel_ingot'},
+		{'default:steel_ingot', 'default:diamond', 'default:steel_ingot'},
+	}
+})
+
 gunslinger.register_gun("gunslinger_rangedweapons:svd", {
 	itemdef = {
 		description = "SVD Sniper Rifle",
@@ -352,6 +490,14 @@ gunslinger.register_gun("gunslinger_rangedweapons:svd", {
 	ammo = "gunslinger_rangedweapons:mag_sniper",
 	fire_sound = "rangedweapons_rifle_b"
 })
+minetest.register_craft({
+	output = 'gunslinger_rangedweapons:svd 1 65534',
+	recipe = {
+		{'default:steel_ingot', 'default:diamondblock', 'default:steel_ingot'},
+		{'default:steel_ingot', 'default:steel_ingot', 'default:steel_ingot'},
+		{'default:tree', 'default:diamond', 'default:tree'},
+	}
+})
 
 --Pistols
 
@@ -373,6 +519,15 @@ gunslinger.register_gun("gunslinger_rangedweapons:beretta", {
 	ammo = "gunslinger_rangedweapons:bullet_9mm 15",
 	fire_sound = "rangedweapons_beretta"
 })
+minetest.register_craft({
+	output = 'gunslinger_rangedweapons:beretta 1 65534',
+	recipe = {
+		{'default:steel_ingot', 'default:steel_ingot', 'default:steel_ingot'},
+		{'', 'default:mese_crystal', 'homedecor:plastic_sheeting'},
+		{'', '', 'homedecor:plastic_sheeting'},
+	}
+})
+
 gunslinger.register_gun("gunslinger_rangedweapons:glock17", {
 	itemdef = {
 		description = "Glock 17 Pistol",
@@ -391,9 +546,18 @@ gunslinger.register_gun("gunslinger_rangedweapons:glock17", {
 	ammo = "gunslinger_rangedweapons:bullet_9mm 15",
 	fire_sound = "rangedweapons_glock"
 })
-gunslinger.register_gun("gunslinger_rangedweapons:marakov", {
+minetest.register_craft({
+	output = 'gunslinger_rangedweapons:glock17 1 65534',
+	recipe = {
+		{'homedecor:plastic_sheeting', 'homedecor:plastic_sheeting', 'homedecor:plastic_sheeting'},
+		{'default:steel_ingot', 'default:steel_ingot', 'default:steel_ingot'},
+		{'', 'default:diamond', 'homedecor:plastic_sheeting'},
+	}
+})
+
+gunslinger.register_gun("gunslinger_rangedweapons:makarov", {
 	itemdef = {
-		description = "Marakov Pistol",
+		description = "Makarov Pistol",
 		inventory_image = "rangedweapons_makarov.png",
 		wield_image = "rangedweapons_makarov.png",
 		--wield_scale = {x = 4, y = 4, z = 1}
@@ -409,6 +573,15 @@ gunslinger.register_gun("gunslinger_rangedweapons:marakov", {
 	ammo = "gunslinger_rangedweapons:bullet_9mm 15",
 	fire_sound = "rangedweapons_makarov"
 })
+minetest.register_craft({
+	output = 'gunslinger_rangedweapons:makarov 1 65534',
+	recipe = {
+		{'', 'default:steel_ingot', 'default:steel_ingot'},
+		{'', 'default:mese_crystal_fragment', 'default:tree'},
+		{'', '', 'dye:black'},
+	}
+})
+
 gunslinger.register_gun("gunslinger_rangedweapons:tec9", {
 	itemdef = {
 		description = "Tec9 Pistol",
@@ -428,6 +601,15 @@ gunslinger.register_gun("gunslinger_rangedweapons:tec9", {
 	ammo = "gunslinger_rangedweapons:mag_smg",
 	fire_sound = "rangedweapons_machine_pistol"
 })
+minetest.register_craft({
+	output = 'gunslinger_rangedweapons:tec9 1 65534',
+	recipe = {
+		{'homedecor:plastic_sheeting', 'homedecor:plastic_sheeting', 'homedecor:plastic_sheeting'},
+		{'default:steel_ingot', 'default:steel_ingot', 'default:steel_ingot'},
+		{'default:steel_ingot', 'default:mese_crystal_fragment', 'default:steel_ingot'},
+	}
+})
+
 gunslinger.register_gun("gunslinger_rangedweapons:luger", {
 	itemdef = {
 		description = "Luger Pistol",
@@ -446,30 +628,96 @@ gunslinger.register_gun("gunslinger_rangedweapons:luger", {
 	ammo = "gunslinger_rangedweapons:bullet_9mm 15",
 	fire_sound = "rangedweapons_makarov"
 })
+minetest.register_craft({
+	output = 'gunslinger_rangedweapons:luger 1 65534',
+	recipe = {
+		{'', 'default:steel_ingot', 'default:steel_ingot'},
+		{'', '', 'default:tree'},
+		{'', '', ''},
+	}
+})
+
+--Bullets
+
 minetest.register_craftitem("gunslinger_rangedweapons:bullet_556mm", {
 	description = "5.56mm Rifle round",
 	inventory_image = "rangedweapons_556mm.png",
 })
+minetest.register_craft({
+	output = 'gunslinger_rangedweapons:bullet_556mm 75',
+	recipe = {
+		{'', 'default:gold_ingot', ''},
+		{'default:gold_ingot', 'tnt:gunpowder', 'default:gold_ingot'},
+		{'default:gold_ingot', 'tnt:gunpowder', 'default:gold_ingot'},
+	}
+})
+
 minetest.register_craftitem("gunslinger_rangedweapons:bullet_12g", {
 	description = "12 Gauge Buckshot",
 	inventory_image = "rangedweapons_12g.png",
 })
+minetest.register_craft({
+	output = 'gunslinger_rangedweapons:bullet_12g 12',
+	recipe = {
+		{'default:bronze_ingot', 'default:steel_ingot', 'default:bronze_ingot'},
+		{'default:bronze_ingot', 'tnt:gunpowder', 'default:bronze_ingot'},
+		{'default:gold_ingot', 'tnt:gunpowder', 'default:gold_ingot'},
+	}
+})
+
 minetest.register_craftitem("gunslinger_rangedweapons:bullet_9mm", {
 	description = "9mm Pistol round",
 	inventory_image = "rangedweapons_9mm.png",
 })
+minetest.register_craft({
+	output = 'gunslinger_rangedweapons:bullet_9mm 30',
+	recipe = {
+		{'default:steel_ingot', '', ''},
+		{'tnt:gunpowder', '', ''},
+		{'default:copper_ingot', '', ''},
+	}
+})
+
 minetest.register_craftitem("gunslinger_rangedweapons:bullet_10mm", {
 	description = "10mm Submachinegun round",
 	inventory_image = "rangedweapons_10mm.png",
 })
+minetest.register_craft({
+	output = 'gunslinger_rangedweapons:bullet_10mm 60',
+	recipe = {
+		{'', 'default:bronze_ingot', ''},
+		{'default:steel_ingot', 'tnt:gunpowder', 'default:steel_ingot'},
+		{'default:steel_ingot', 'tnt:gunpowder', 'default:steel_ingot'},
+	}
+})
+
 minetest.register_craftitem("gunslinger_rangedweapons:bullet_762mm", {
 	description = "7.62mm Heavy Rifle round",
 	inventory_image = "rangedweapons_762mm.png",
 })
+minetest.register_craft({
+	output = 'gunslinger_rangedweapons:bullet_762mm 50',
+	recipe = {
+		{'default:bronze_ingot', 'tnt:gunpowder', 'default:bronze_ingot'},
+		{'default:gold_ingot', 'tnt:gunpowder', 'default:gold_ingot'},
+		{'default:gold_ingot', 'tnt:gunpowder', 'default:gold_ingot'},
+	}
+})
+
 minetest.register_craftitem("gunslinger_rangedweapons:bullet_308mm", {
 	description = ".308mm Sniper Rifle round",
 	inventory_image = "rangedweapons_308winchester.png",
 })
+minetest.register_craft({
+	output = 'gunslinger_rangedweapons:bullet_308mm 15',
+	recipe = {
+		{'', 'default:steel_ingot', ''},
+		{'default:bronze_ingot', 'tnt:gunpowder', 'default:bronze_ingot'},
+		{'default:gold_ingot', 'tnt:gunpowder', 'default:gold_ingot'},
+	}
+})
+
+--Magazines
 
 minetest.register_tool("gunslinger_rangedweapons:mag_stanag", {
 	description = "STANAG Magazine",
